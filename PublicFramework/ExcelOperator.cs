53d01ed8a4049f2f6bbb31572795dee847b7fce6
JuniorCai <JuniorCai@outlook.com>
2018-08-10 09:37:30 +0800

    添加项目文件。

---
+++ PublicFramework/ExcelOperator.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+
+namespace PublicFramework
+{
+    public class ExcelOperator
+    {
+        public string FilePath { get; set; }
+
+        public string FileName { get; set; }
+
+        public int RowCount { get; set; }
+
+        public int ColumnCount { get; set; }
+
+        public List<OriginalWord> OriginalWords { get; set; }
+
+        public List<string> RootWords { get; set; }
+
+        public List<string> PrefixWords { get; set; }
+
+        public List<string> SuffixWords { get; set; }
+
+
+        public Microsoft.Office.Interop.Excel.Application app;
+        public Microsoft.Office.Interop.Excel.Workbooks wbs;
+        public Microsoft.Office.Interop.Excel.Workbook wb;
+        public Microsoft.Office.Interop.Excel.Worksheets wss;
+        public Microsoft.Office.Interop.Excel.Worksheet ws;
+
+
+        public ExcelOperator(string filePath,string rootWords)
+        {
+            this.FilePath = filePath;
+            this.RootWords = rootWords.Split(',').ToList();
+        }
+
+        public void Open()//打开一个Microsoft.Office.Interop.Excel文件
+        {
+            app = new Microsoft.Office.Interop.Excel.Application();
+            wbs = app.Workbooks;
+            wb = wbs.Add(FilePath);
+            ws = (Microsoft.Office.Interop.Excel.Worksheet)wb.Worksheets["Sheet0"];
+
+            this.RowCount = ws.UsedRange.Rows.Count;
+            this.ColumnCount = ws.UsedRange.Columns.Count;
+            FileName = FilePath;
+        }
+
+        public void Read()
+        {
+            OriginalWords = new List<OriginalWord>();
+            Array rangeValue = (this.ws.Range[ws.Cells[2, 1], ws.Cells[this.RowCount, 3]]).Value2 as Array;
+            //Microsoft.Office.Interop.Excel.Range range2 = this.ws.Range[ws.Cells[2, 3], ws.Cells[this.RowCount, 3]];
+
+            for (int index = 1; index < RowCount-1; index++)
+            {
+                OriginalWords.Add(new OriginalWord(){Word = rangeValue.GetValue(index,1).ToString(),DailySearchCount = int.Parse(rangeValue.GetValue(index,3).ToString())});
+            }
+        }
+
+        public void ApartOriginalWords()
+        {
+            foreach (string rootWord in RootWords)
+            {
+                int rootWordLength = rootWord.Length;
+                if(rootWordLength==0)
+                    break;
+                foreach (OriginalWord originalWord in OriginalWords)
+                {
+                    int originalWordLength = originalWord.Word.Length;
+                    originalWord.RootWord = rootWord;
+
+                    if (originalWordLength > 0 && originalWord.Word.Contains(rootWord))
+                    {
+                        int index = originalWord.Word.IndexOf(rootWord);
+                        if (index == 0)
+                        {
+                            originalWord.PrefixWord = "";
+                            originalWord.SuffixWord = originalWord.Word;
+                        }else if (originalWordLength - index == rootWordLength)
+                        {
+                            originalWord.PrefixWord = originalWord.Word;
+                            originalWord.SuffixWord = "";
+                        }
+                        else
+                        {
+                            originalWord.PrefixWord = originalWord.Word.Substring(0,index+rootWordLength);
+                            originalWord.SuffixWord = originalWord.Word.Substring(index);
+                        }
+                    }
+                }
+
+                OriginalWords = OriginalWords.Where(o => o.PrefixWord != null&&o.SuffixWord!=null).ToList();
+            }
+        }
+
+        public void CountSearch()
+        {
+
+        }
+
+
+        public void Close()
+            //关闭一个Microsoft.Office.Interop.Excel对象，销毁对象
+        {
+            //wb.Save();
+            wb.Close(Type.Missing, Type.Missing, Type.Missing);
+            wbs.Close();
+            app.Quit();
+            wb = null;
+            wbs = null;
+            app = null;
+            GC.Collect();
+        }
+
+    }
+
+    public class OriginalWord
+    {
+        public string Word { get; set; }
+
+        public int DailySearchCount { get; set; }
+
+        public string PrefixWord { get; set; }
+
+        public string SuffixWord { get; set; }
+
+        public string RootWord { get; set; }
+    }
+
+    public class CountSearch
+    {
+        public string WordType { get; set; }
+
+        public string FixWord { get; set; }
+
+        public int WordCount { get; set; }
+
+        public int WordSearch { get; set; }
+
+
+    }
+}
---
+++ SeoTool/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PublicFramework;
+
+namespace SeoTool
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            openFileDialog.Title = "选择文件";
+            openFileDialog.Filter = "Excel文件(*.xls,*.xlsx)|*.xls;*.xlsx";
+            openFileDialog.FileName = "选择文件夹.";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.ValidateNames = false;
+            openFileDialog.CheckFileExists = false;
+            openFileDialog.CheckPathExists = true;
+            openFileDialog.Multiselect = false; //允许同时选择多个文件 
+            bool? result = openFileDialog.ShowDialog();
+            if (result != true)
+            {
+                return;
+            }
+            else
+            {
+                string[] files = openFileDialog.FileNames;
+                if (files.Length > 0)
+                {
+                    filePath.Text = files[0];
+
+                    
+                }
+
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string fileUrl = filePath.Text;
+
+
+            ExcelOperator excelOperator = new ExcelOperator(fileUrl, rootWord.Text);
+            excelOperator.Open();
+            excelOperator.Read();
+            excelOperator.ApartOriginalWords();
+
+            excelOperator.Close();
+        }
+    }
+}

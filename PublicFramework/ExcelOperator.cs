317a730cc1b541214574857460d67e3af60d223b
JuniorCai <JuniorCai@outlook.com>
2018-08-10 12:16:11 +0800

    完善功能

--- PublicFramework/ExcelOperator.cs
+++ PublicFramework/ExcelOperator.cs
@@ -11,6 +11,8 @@ namespace PublicFramework
     {
         public string FilePath { get; set; }
 
+        public string SaveDirectoryPath { get; set; }
+
         public string FileName { get; set; }
 
         public int RowCount { get; set; }
@@ -33,9 +35,12 @@ namespace PublicFramework
         public Microsoft.Office.Interop.Excel.Worksheet ws;
 
 
-        public ExcelOperator(string filePath,string rootWords)
+        public ExcelOperator(string directoryPath,string filePath,string rootWords)
         {
             this.FilePath = filePath;
+            this.SaveDirectoryPath = directoryPath.LastIndexOf(@"\") == (directoryPath.Length - 1)
+                ? directoryPath.Substring(0, directoryPath.Length - 1)
+                : directoryPath;
             this.RootWords = rootWords.Split(',').ToList();
         }
 
@@ -44,26 +49,30 @@ namespace PublicFramework
             app = new Microsoft.Office.Interop.Excel.Application();
             wbs = app.Workbooks;
             wb = wbs.Add(FilePath);
-            ws = (Microsoft.Office.Interop.Excel.Worksheet)wb.Worksheets["Sheet0"];
+            ws = (Microsoft.Office.Interop.Excel.Worksheet)wb.Worksheets["Sheet1"];
 
             this.RowCount = ws.UsedRange.Rows.Count;
             this.ColumnCount = ws.UsedRange.Columns.Count;
             FileName = FilePath;
         }
 
+
+        #region 分词
+
         public void Read()
         {
             OriginalWords = new List<OriginalWord>();
-            Array rangeValue = (this.ws.Range[ws.Cells[2, 1], ws.Cells[this.RowCount, 3]]).Value2 as Array;
+            Array rangeValue = (this.ws.Range[ws.Cells[1, 1], ws.Cells[this.RowCount, 3]]).Value2 as Array;
             //Microsoft.Office.Interop.Excel.Range range2 = this.ws.Range[ws.Cells[2, 3], ws.Cells[this.RowCount, 3]];
 
-            for (int index = 1; index < RowCount-1; index++)
+            for (int index = 1; index <= RowCount; index++)
             {
                 OriginalWords.Add(new OriginalWord(){Word = rangeValue.GetValue(index,1).ToString(),DailySearchCount = int.Parse(rangeValue.GetValue(index,3).ToString())});
             }
+            Close();
         }
 
-        public void ApartOriginalWords()
+        public List<OriginalWord> ApartOriginalWords()
         {
             foreach (string rootWord in RootWords)
             {
@@ -78,30 +87,142 @@ namespace PublicFramework
                     if (originalWordLength > 0 && originalWord.Word.Contains(rootWord))
                     {
                         int index = originalWord.Word.IndexOf(rootWord);
-                        if (index == 0)
-                        {
-                            originalWord.PrefixWord = "";
-                            originalWord.SuffixWord = originalWord.Word;
-                        }else if (originalWordLength - index == rootWordLength)
-                        {
-                            originalWord.PrefixWord = originalWord.Word;
-                            originalWord.SuffixWord = "";
-                        }
-                        else
-                        {
-                            originalWord.PrefixWord = originalWord.Word.Substring(0,index+rootWordLength);
-                            originalWord.SuffixWord = originalWord.Word.Substring(index);
-                        }
+                        originalWord.PrefixWord = originalWord.Word.Substring(0, index + rootWordLength);
+                        originalWord.SuffixWord = originalWord.Word.Substring(index);
+
+                    }
+                    else
+                    {
+                        string item = originalWord.Word;
                     }
                 }
 
                 OriginalWords = OriginalWords.Where(o => o.PrefixWord != null&&o.SuffixWord!=null).ToList();
             }
+            return OriginalWords;
         }
+
+
+        public void FlushApartResultToFile(List<OriginalWord> apartResult)
+        {
+            app = new Microsoft.Office.Interop.Excel.Application();
+            wbs = app.Workbooks;
+            wb = wbs.Add(true);
+
+            Microsoft.Office.Interop.Excel.Worksheet ws = (Microsoft.Office.Interop.Excel.Worksheet)wb.Worksheets["Sheet1"];
+
+            for (int i = 1; i <= apartResult.Count; i++)
+            {
+                ws.Cells[i, 1] = apartResult[i - 1].Word;
+                ws.Cells[i, 2] = apartResult[i - 1].DailySearchCount;
+                ws.Cells[i, 3] = apartResult[i - 1].PrefixWord;
+                ws.Cells[i, 4] = apartResult[i - 1].SuffixWord;
+                ws.Cells[i, 5] = apartResult[i - 1].RootWord;
+
+            }
+            SaveAs(RootWords[0]+"分词");
+            Close();
+        }
+
+
+        #endregion
+
+
+        #region 统计
+
+        public void ReadApartResult()
+        {
+            OriginalWords = new List<OriginalWord>();
+            Array rangeValue = (this.ws.Range[ws.Cells[1, 1], ws.Cells[this.RowCount, 5]]).Value2 as Array;
+            //Microsoft.Office.Interop.Excel.Range range2 = this.ws.Range[ws.Cells[2, 3], ws.Cells[this.RowCount, 3]];
+
+            for (int index = 1; index <= RowCount; index++)
+            {
+                OriginalWords.Add(new OriginalWord()
+                {
+                    Word = rangeValue.GetValue(index, 1).ToString(),
+                    DailySearchCount = int.Parse(rangeValue.GetValue(index, 2).ToString()),
+                    PrefixWord = rangeValue.GetValue(index, 3).ToString(),
+                    SuffixWord = rangeValue.GetValue(index, 4).ToString(),
+                    RootWord = rangeValue.GetValue(index, 5).ToString()
+                });
+            }
+            Close();
+        }
+
 
         public void CountSearch()
         {
+            var beforeQuery = from originalWord in OriginalWords
+                group originalWord by originalWord.PrefixWord
+                into g
+                select new CountSearch()
+                {
+                    WordType = "Before",
+                    FixWord = g.Key,
+                    WordCount = g.Count(),
+                    WordSearch = g.Sum(item => item.DailySearchCount)
+                };
+            var beforeList = beforeQuery.ToList();
 
+            var afterQuery = from originalWord in OriginalWords
+                group originalWord by originalWord.SuffixWord
+                into g
+                select new CountSearch()
+                {
+                    WordType = "After",
+                    FixWord = g.Key,
+                    WordCount = g.Count(),
+                    WordSearch = g.Sum(item => item.DailySearchCount)
+                };
+            var afterList = afterQuery.ToList();
+            beforeList.AddRange(afterList);
+            beforeList.Sort((a, b) => -a.WordSearch.CompareTo(b.WordSearch));
+
+            FlushToFile(beforeList);
+
+        }
+
+        private void FlushToFile(List<CountSearch> finalList)
+        {
+            app = new Microsoft.Office.Interop.Excel.Application();
+            wbs = app.Workbooks;
+            wb = wbs.Add(true);
+
+            Microsoft.Office.Interop.Excel.Worksheet ws =
+                (Microsoft.Office.Interop.Excel.Worksheet) wb.Worksheets["Sheet1"];
+
+            for (int i = 1; i <= finalList.Count; i++)
+            {
+                ws.Cells[i, 1] = finalList[i - 1].WordType;
+                ws.Cells[i, 2] = finalList[i - 1].FixWord;
+                ws.Cells[i, 3] = finalList[i - 1].WordCount;
+                ws.Cells[i, 4] = finalList[i - 1].WordSearch;
+
+            }
+            SaveAs("词缀统计");
+            Close();
+        }
+
+        #endregion
+
+
+
+        //文档另存为
+        public bool SaveAs(object fileName)
+        {
+            try
+            {
+                wb.SaveAs(SaveDirectoryPath + @"\" + fileName, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+                return true;
+
+            }
+
+            catch (Exception ex)
+            {
+                return false;
+
+            }
         }
 
 
--- SeoTool/MainWindow.xaml.cs
+++ SeoTool/MainWindow.xaml.cs
@@ -57,15 +57,71 @@ namespace SeoTool
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
+            ApartResult.Content = "分词进度：" + "正在分词，请稍后";
+
             string fileUrl = filePath.Text;
+            string directoryPath = this.directoryPath.Text.Trim();
+            if (directoryPath.Length == 0)
+            {
+                DirectoryResult.Content = "保存目录未填写";
+                return;
+            }
 
-
-            ExcelOperator excelOperator = new ExcelOperator(fileUrl, rootWord.Text);
+            ExcelOperator excelOperator = new ExcelOperator(directoryPath, fileUrl, rootWord.Text);
             excelOperator.Open();
             excelOperator.Read();
-            excelOperator.ApartOriginalWords();
+            var apartResultList = excelOperator.ApartOriginalWords();
+            excelOperator.FlushApartResultToFile(apartResultList);
+            ApartResult.Content = "分词进度：" + "分词结束，请查看文件";
 
-            excelOperator.Close();
+            //excelOperator.CountSearch();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            string fileUrl = countFile.Text;
+            string directoryPath = this.directoryPath.Text.Trim();
+            if (directoryPath.Length == 0)
+            {
+                DirectoryResult.Content = "保存目录未填写";
+                return;
+            }
+            ExcelOperator excelOperator = new ExcelOperator(directoryPath, fileUrl, rootWord.Text);
+            excelOperator.Open();
+            excelOperator.ReadApartResult();
+            excelOperator.CountSearch();
+            CountResult.Content = "统计进度：" + "统计结束，请查看文件";
+
+            //excelOperator.CountSearch();
+        }
+
+        private void CountChooseFile_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            openFileDialog.Title = "选择文件";
+            openFileDialog.Filter = "Excel文件(*.xls,*.xlsx)|*.xls;*.xlsx";
+            openFileDialog.FileName = "选择文件夹.";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.ValidateNames = false;
+            openFileDialog.CheckFileExists = false;
+            openFileDialog.CheckPathExists = true;
+            openFileDialog.Multiselect = false; //允许同时选择多个文件 
+            bool? result = openFileDialog.ShowDialog();
+            if (result != true)
+            {
+                return;
+            }
+            else
+            {
+                string[] files = openFileDialog.FileNames;
+                if (files.Length > 0)
+                {
+                    countFile.Text = files[0];
+
+
+                }
+
+            }
         }
     }
 }
